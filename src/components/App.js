88658fc9343d81f3f392ec6c774d09293b6a838d
Rainer Brieger <hello@rainerb.com>
2020-12-08 10:41:52 -0700

    Added updating state section

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,29 @@ import Order from './Order';
 import Inventory from './Inventory'; 
 
 class App extends React.Component {
+    state = {
+        fishes: {},
+        order: {}
+    };
+    addFish = fish => {
+        // To update State!
+        // 1. Take copy of exisiting State
+        const fishes = {...this.state.fishes};
+        // 2. Add new State to variable
+        fishes[`fish${Date.now()}`] = fish;
+        // 3. Adding new updated State into State
+        this.setState({
+            fishes
+        });
+    };
     render() {
         return (
             <div className= "catch-of-the-day">
                 <div className="menu">
                     <Header tagline="FRESH SEAFOOD MARKET" />
                 </div>
-            <Inventory />
-            <Order />
+                <Inventory addFish = {this.addFish} />
+                <Order />
             </div>
         );
     }
