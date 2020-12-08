3dd9a9ce3ac5224021e0f927b1d68d0cf8be4e49
Rainer Brieger <hello@rainerb.com>
2020-12-08 09:29:08 -0700

    added handling events section

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
+import Router from './components/Router';
+import './css/style.css';
 
-class StorePicker extends React.Component {
-    render() {
-        return <p>Hello!</p>
-    }
-}
-
-render(<p>Hello!</p>, document.querySelector('#main'));
+render(<Router />, document.querySelector('#main'));
