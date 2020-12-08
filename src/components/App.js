a30e9fa4a5ee94e3612020427e6d1419b1ee3567
Rainer Brieger <hello@rainerb.com>
2020-12-08 14:07:40 -0700

    Display order Stte with JSX

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory'; 
+import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
 
 class App extends React.Component {
     state = {
@@ -19,14 +21,39 @@ class App extends React.Component {
             fishes
         });
     };
+    loadSampleFishes = () => {
+        this.setState({
+            fishes: sampleFishes
+        });
+    };
+
+    addToOrder = (key) => {
+        // To add to order
+        // 1. Take copy of State
+        const order = {...this.state.order}
+        // 2. Either add to the order or update the number in order
+        order[key] = order[key] + 1 || 1;
+        //  3. Call setState to update State Object
+        this.setState({ order })
+    }
+
     render() {
         return (
             <div className= "catch-of-the-day">
                 <div className="menu">
                     <Header tagline="FRESH SEAFOOD MARKET" />
+                    <ul className="fishes">
+                        {Object.keys(this.state.fishes).map(key => (
+                        <Fish 
+                        key={key} 
+                        index={key}
+                        details= {this.state.fishes[key]} 
+                        addToOrder= {this.addToOrder} 
+                        />))}
+                    </ul>
                 </div>
-                <Inventory addFish = {this.addFish} />
-                <Order />
+                <Inventory addFish = {this.addFish} loadSampleFishes={this.loadSampleFishes} />
+                <Order fishes={this.state.fishes} order={this.state.order} />
             </div>
         );
     }
